3000aaaabc90ff4ec6bf27804054d6b1c7c80c00
lucaisacoder <lucas_hsueh@foxmail.com>
2020-09-13 22:12:22 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -204,8 +204,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template1 Documentation',
+   u'Read the Docs2', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -234,8 +234,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'readthedocstemplate3', u'Read the Docs Template4 Documentation',
+     [u'Read the Docs5'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -248,8 +248,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template6 Documentation',
+   u'Read the Docs', 'ReadtheDocsTemplate7', 'One line description of project.',
    'Miscellaneous'),
 ]
 
